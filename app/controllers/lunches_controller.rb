f7569c9f45c4bfa991c4b84675ffdf2d47f9a8d8
NoamSP <noamsp@gmail.com>
2014-01-24 14:03:54 +0200

    Created rails and git

---
+++ app/controllers/lunches_controller.rb
@@ -0,0 +1,4 @@
+class LunchesController < ApplicationController
+  def home
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchin2::Application.config.secret_token = 'ad719b8816675154effcb4d68293279849272522c96ef2c35192bb7cdbcdb951feb11c1f88725ac4bd58e150c64583c6ac677b902b1503ea03f122482deb882b'
