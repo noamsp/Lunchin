35c2c80c556d9eb8091ebd8ea974e101b0bc9b3e
NoamSP <noamsp@gmail.com>
2014-01-24 15:35:07 +0200

    Faceeoob Connect integration - Added DB

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
+#Need to add secret key
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '195968327276430', '6c8fc8d825b0f57aa3772949025719b8'
 end
